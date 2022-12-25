79aaafc8258e887d488fe05d3eff8d23912b0d06
LorDawid <szostak.d7@gmail.com>
2022-12-25 14:53:43 +0100

    nowa strona projektu

--- main.js
+++ main.js
@@ -15,12 +15,5 @@ function placeSnapper() {
   snapper.style.top = globalPosition - visualViewport.height + "px";
 }
 
-window.addEventListener("load",function() {
-  setTimeout(function(){
-      // This hides the address bar:
-      window.scrollTo(0, 1);
-  }, 0);
-});
-
 addEventListener("resize", placeSnapper)
 placeSnapper()
